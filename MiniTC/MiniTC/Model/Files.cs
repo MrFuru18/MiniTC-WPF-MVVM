8e33aff0c8a16fbd74b33fd2d984836e4053d01d
MrFuru18 <47296815+MrFuru18@users.noreply.github.com>
2021-06-19 23:03:24 +0200

    Add files via upload

---
+++ MiniTC/MiniTC/Model/Files.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MiniTC.Model
+{
+    class Files
+    {
+        public string[] Get_Drives()
+        {
+            string[] drives = Directory.GetLogicalDrives();
+            return drives;
+        }
+        public string[] Get_Paths(string path, bool d)
+        {
+            List<string> list = new List<string>();
+            if (d == false)
+                list.Add("...");
+            string[] directories = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
+            foreach (string dir in directories)
+            {
+                int len = dir.Length - path.Length;
+                string name  = dir.Substring(dir.Length - len);
+                name = "<D>" + name;
+                list.Add(name);
+            }
+
+            foreach (string file in files) 
+            {
+                int len = file.Length - path.Length;
+                string name = file.Substring(file.Length - len);
+                list.Add(Path.GetFileName(name));
+            }
+
+            string[] paths = list.ToArray();
+            return paths;
+        }
+
+        public void CopyFile(string file, string directory)
+        {
+            File.Copy(file, directory, true);
+        }
+    }
+}
---
+++ MiniTC/MiniTC/Model/ManageDirectories.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTC.Model
+{
+    class ManageDirectories
+    {
+        List<string> directories = new List<string>();
+
+        public void Clear_Directories()
+        {
+            directories.Clear();
+        }
+        public void Add_Directory(string p)
+        {
+            directories.Add(p);
+        }
+        public void Delete_Last_Directory()
+        {
+            if (directories.Any()) //prevent IndexOutOfRangeException for empty list
+            {
+                directories.RemoveAt(directories.Count - 1);
+            }
+        }
+
+        public string Get_Last_Directory()
+        {
+            string[] paths = directories.ToArray();
+            string last = "";
+            foreach (string dir in paths)
+                last = dir;
+            
+            return last;
+        }
+        public bool Go_back(string p)
+        {
+            if (p == "...")
+                return true;
+            return false;
+        }
+        public bool Only_Drive()
+        {
+            int n = directories.Count();
+            if (n == 1)
+                return true;
+            return false;
+        }
+        public string Full_Name(string name)
+        {
+            string newname = Get_Last_Directory();
+            newname = newname + name.Substring(name.Length - (name.Length - 3));
+            return newname;
+        }
+
+    }
+}
